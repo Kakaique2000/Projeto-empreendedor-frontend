f514268404b935aa85b638331292adc7d38835d2
Kakaique2000 <kaique.lisboa@gmail.com>
2020-08-29 20:07:50 -0300

    feat(app.component): base do programa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projeto-emprego-frontend';
+  sideMenuOpened = false;
+
+  get menuIcone() {
+    return (this.sideMenuOpened && 'menu_open') || 'menu' 
+  }
+
+  clickMenu() {
+  this.sideMenuOpened = !this.sideMenuOpened
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from 'src/shared/shared.module';
+import { AppRoutingModule } from './app.routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
+    DashboardComponent,
     AppComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    SharedModule,
+    BrowserModule,
+    HomeModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
