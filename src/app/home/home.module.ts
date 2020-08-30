d48cf78489ac33f927108098a2e60dfd759a7296
Kakaique2000 <kaique.lisboa@gmail.com>
2020-08-30 11:59:11 -0300

    feat(search-card): layout search card

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { HomeComponent } from './home.component';
+import { SharedModule } from 'src/shared/shared.module';
+import { SearchCardComponent } from './search-card/search-card.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, SearchCardComponent],
   imports: [
+    FormsModule,
+    SharedModule,
     CommonModule
   ]
 })
