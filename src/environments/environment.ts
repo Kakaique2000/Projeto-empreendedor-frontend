dcbb3a683fd6a0c95cb6ac7db9c10781240ba106
Jose Roberto Correa <jroberto.cr1@gmail.com>
2020-10-07 20:06:04 -0300

    nova versao

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  api: 'http://localhost:3000'
+  production: true,
+  api: 'http://ec2-3-225-72-167.compute-1.amazonaws.com:8080/'
 };
 
 /*
